8505343b2a7d67d0348efdaafc98d28eee05ca4b
Ekremk95 <ekrem3333@gmail.com>
2017-09-07 05:32:57 +0300

    node video stream

---
+++ node/videostream/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const Log = require('log');
+const log = new Log('debug');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use('/io', express.static(__dirname + '/node_modules/socket.io-client/dist/'));
+
+app.get('/', (req, res) => {
+  res.redirect('index.html');
+});
+
+app.get('/video', (req, res) => {
+  res.redirect('viewer.html');
+});
+
+io.on('connection', socket => {
+  socket.on('stream', image => {
+    socket.broadcast.emit('stream', image);
+  });
+});
+
+http.listen(port, () => {
+  log.info('Listening');
+});
