cad7a85fa10306e4b58daeb64a4b49bc442b9ba3
Ekrem <ekremk95@gmail.com>
2018-07-01 19:27:53 +0300

    remove vue

--- videostream/2/router.js
+++ videostream/2/router.js
@@ -4,22 +4,20 @@ const path = require('path');
 const redis = require('redis');
 const client = redis.createClient();
 
-client.on('error', function (err) {
+client.on('error', function(err) {
     console.log('Error ' + err);
-  });
+});
 
 router.get('/streams', (req, res) => {
-
     client.smembers('test_online_users', (err, reply) => {
-      if (err)return err;
+        if (err) return err;
 
-      // res.json({ streams: JSON.parse(reply) });
-      res.send(reply);
+        res.send(reply);
     });
-  });
+});
 
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './template', 'index.html'));
+    res.sendFile(path.join(__dirname, './template', 'index.html'));
 });
 
 module.exports = router;
---
+++ videostream/2/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+const dev = [];
+const production = [new UglifyJSPlugin()];
+
+const APP_DIR = path.resolve(__dirname, 'app');
+const BUILT_DIR = path.resolve(__dirname, 'public');
+
+const config = {
+    entry: APP_DIR + '/app.js',
+    output: {
+        path: BUILT_DIR,
+        filename: 'bundle.js',
+    },
+    devtool: production ? '' : 'inline-sourcemap',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: APP_DIR,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react'],
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ],
+            },
+        ],
+    },
+    plugins: production ? production : dev,
+};
+
+module.exports = config;
